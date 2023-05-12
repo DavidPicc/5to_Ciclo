3ad6d42494cf3efa63efcd64ad93cb02e7cc21a1
LuisZapata12 <luis.98.30.16@gmail.com>
2023-05-12 17:57:20 -0500

    12/05 Vfx PlayerSmoke / Vfx interactuable explosivo

---
+++ Assets/VFX/Player/Smoke_action.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Smoke_action : MonoBehaviour
+{
+    public GameObject Firepoint;
+    public GameObject vfx;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       Instantiate (vfx, Firepoint.transform.position, Quaternion.identity);
+    }
+}
