114afd2cfaa65603872e0bcae7afe834c201a841
bradymelendez <105991377+bradymelendez@users.noreply.github.com>
2023-05-27 15:02:45 -0500

    Revert "25/05-destruccion de bala y VFX"
    This reverts commit ae55db6545a17738a55d2b2afbc3bc21a97835e4.

--- Assets/Scripts/Player/PlayerBullet_Script.cs
+++ Assets/Scripts/Player/PlayerBullet_Script.cs
@@ -6,11 +6,6 @@ public class PlayerBullet_Script : MonoBehaviour
 {
     [SerializeField] public float damage;
     [SerializeField] public float dissapearAfterColliding;
-
-    private void Start()
-    {
-        Destroy(this.gameObject, 4f);
-    }
     private void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Obstacle"))
@@ -28,7 +23,7 @@ public class PlayerBullet_Script : MonoBehaviour
                     other.GetComponent<Enemy_Health>().TakeDamage(damage);
                 }
                 Destroy(gameObject, dissapearAfterColliding);
-               
+                Destroy(gameObject, 2.5f);
             }
         }
         if (other.CompareTag("EnemyBullet"))
