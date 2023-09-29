1a0af967b6e8690065ac94f46b8ad452f8e9ccd3
Said Albujar Torres <saidalbujar23@gmail.com>
2023-09-29 01:50:06 -0500

    29/09 Enemigo 5 cambio de disparo

---
+++ Assets/Scripts/Enemies/Shoot_Flame.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot_Flame : EnemyShooting
+{
+    Transform _camera;
+
+    [Header("General")]
+    public GameObject bulletPrefab;
+    public float damage;
+    public float bulletSpeed;
+    public float fireRate;
+    public float bulletNumber;
+    public float bulletDisappear;
+    public float bulletAng;
+    public float anticipation;
+    [SerializeField] public float maxAngle;
+    [SerializeField] public Transform[] shootPoints;
+    [Header("Shooting")]
+    public Transform pointShoot;
+    [SerializeField] public AudioSource audioManager;
+    [SerializeField] public AudioClip ShootSound;
+    private float shootTimer;
+    private bool shotBullet;
+    public float distanceToShoot;
+    //bool canShoot => !shotBullet && Mathf.Abs(transform.localPosition.x) <= distanceToShoot;
+    bool canShoot => !shotBullet && Mathf.Abs(Vector3.Distance(_camera.position, transform.position)) <= distanceToShoot;
+    public GameObject antiSphere;
+    void Start()
+    {
+        _camera = FindObjectOfType<StageMovement>().transform;
+        antiSphere.SetActive(false);
+    }
+
+    void Update()
+    {
+        Shoot();
+
+    }
+
+    void CheckIfShot()
+    {
+        if (shootTimer <= 0)
+        {
+            shotBullet = false;
+        }
+
+        if (shotBullet)
+        {
+            shootTimer -= Time.deltaTime;
+        }
+        else
+        {
+            shootTimer = fireRate;
+        }
+    }
+
+    void Shoot()
+    {
+        CheckIfShot();
+        Anticipation();
+        if (canShoot)
+        {
+            antiSphere.SetActive(false);
+            ShootAngular();
+        }
+    }
+
+    void Anticipation()
+    {
+        if (!canShoot)
+        {
+            antiSphere.SetActive(false);
+        }
+        if (shootTimer <= anticipation)
+        {
+            //Play ANTICIPATION animation.
+            antiSphere.SetActive(true);
+        }
+    }
+
+    void ShootAngular()
+    {
+       /* float angle = 0;
+        if (bulletNumber > 1)
+        {
+            angle -= (bulletAng) / 2;
+        }
+       */
+        for (int i = 0; i < bulletNumber; i++)
+        {
+            float angle;
+            angle = Random.Range(-maxAngle, maxAngle);
+            Quaternion offsetVector = Quaternion.Euler(0, 0, angle);
+            var bullet = Instantiate(bulletPrefab, shootPoints[0].position, shootPoints[0].rotation * offsetVector);
+            bullet.GetComponent<Rigidbody>().AddForce(bullet.transform.right * bulletSpeed, ForceMode.Impulse);
+            Destroy(bullet, bulletDisappear);
+            audioManager.PlayOneShot(ShootSound);
+        }
+
+        shotBullet = true;
+    }
+}
