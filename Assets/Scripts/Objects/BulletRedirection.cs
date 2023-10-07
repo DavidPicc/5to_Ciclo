e8a9526f9a2b89c86ddc19247eb357a8bcc37110
DavidPicc <89850236+DavidPicc@users.noreply.github.com>
2023-10-07 04:51:50 -0500

    07/10 - Multidisparo

--- Assets/Scripts/Objects/BulletRedirection.cs
+++ Assets/Scripts/Objects/BulletRedirection.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class BulletRedirection : MonoBehaviour
 {
+    [SerializeField] bool multiShoot = false;
+    [SerializeField] Transform sphereTransform;
     private void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("PlayerBullet"))
@@ -12,6 +14,15 @@ public class BulletRedirection : MonoBehaviour
             other.transform.right = transform.right;
             Vector3 localRight = other.transform.localRotation * Vector3.right;
             other.GetComponent<Rigidbody>().velocity = localRight * other.GetComponent<Rigidbody>().velocity.magnitude;
+            if(multiShoot)
+            {
+                Quaternion newRot = Quaternion.Euler(other.transform.rotation.x, other.transform.rotation.y, other.transform.rotation.z - 15f);
+                var bull1 = Instantiate(other.gameObject, sphereTransform.position, newRot);
+                bull1.GetComponent<Rigidbody>().velocity = bull1.transform.up * other.GetComponent<Rigidbody>().velocity.magnitude;
+                newRot = Quaternion.Euler(other.transform.rotation.x, other.transform.rotation.y, other.transform.rotation.z + 15f);
+                var bull2 = Instantiate(other.gameObject, sphereTransform.position, newRot);
+                bull2.GetComponent<Rigidbody>().velocity = bull2.transform.up * other.GetComponent<Rigidbody>().velocity.magnitude;
+            }
         }
     }
 }
