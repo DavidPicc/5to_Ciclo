4c0c9d367f0f8554c03b43369c748aa6e4297788
DavidPicc <89850236+DavidPicc@users.noreply.github.com>
2023-11-23 12:18:57 -0500

    23/11 - Importación de elementos nuevos de tienda.

---
+++ Assets/ShopSFX.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopSFX : MonoBehaviour
+{
+    [SerializeField] AudioSource audioSource;
+    [SerializeField] AudioClip buySFX, declineSFX, equipSFX, navSFX, backSFX;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+    public void BuySFX()
+    {
+        AudioManager.instance.PlaySFX(audioSource, buySFX, 1f);
+    }
+
+    public void DeclineSFX()
+    {
+        AudioManager.instance.PlaySFX(audioSource, declineSFX, 1f);
+    }
+
+    public void EquipSFX()
+    {
+        AudioManager.instance.PlaySFX(audioSource, equipSFX, 1f);
+    }
+
+    public void NavSFX()
+    {
+        AudioManager.instance.PlaySFX(audioSource, navSFX, 1f);
+    }
+
+    public void BackSFX()
+    {
+        AudioManager.instance.PlaySFX(audioSource, backSFX, 1f);
+    }
+}
