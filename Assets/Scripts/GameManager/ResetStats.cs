2713e55d63c1b27bd1892860d6a7b3df356caff8
DavidPicc <89850236+DavidPicc@users.noreply.github.com>
2023-11-24 04:32:10 -0500

    24/11 - Tienda Background y animación de entrar a la tienda (solo assets, no implementado)

--- Assets/Scripts/GameManager/ResetStats.cs
+++ Assets/Scripts/GameManager/ResetStats.cs
@@ -4,21 +4,17 @@ using UnityEngine;
 
 public class ResetStats : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         if(GameScore.instance != null)
         {
             Destroy(GameScore.instance.gameObject);
             GameScore.instance = null;
+        }
+        if(UpgradeTracker.instance != null)
+        {
             Destroy(UpgradeTracker.instance.gameObject);
             UpgradeTracker.instance = null;
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
