2913486bff023c52a16823edb624931e375aeabc
DoomXAlpaca <doomxalpaca@gmail.com>
2023-12-12 00:18:47 -0500

    Se agregó un video que se reproduce si el jugador no hace nada en el menú por más de 30 segundos.
    Paolo Pajares

---
+++ Assets/IdleVideo.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IdleVideo : MonoBehaviour
+{
+    float cronometro = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cronometro = 30;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cronometro -= 1 * Time.deltaTime;
+
+        if (Input.anyKeyDown)
+        {
+            cronometro = 30;
+        }
+            
+        if (cronometro <= 0)
+        {
+            SceneManager.LoadScene("VideoIdle");
+        }
+    }
+}
