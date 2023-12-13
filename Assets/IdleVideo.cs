c2a61955684ace0f761901d3f2969b7da4985531
Said Albujar Torres <saidalbujar23@gmail.com>
2023-12-13 01:43:37 -0500

    13/12 Correcion video menu principal

--- Assets/IdleVideo.cs
+++ Assets/IdleVideo.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class IdleVideo : MonoBehaviour
 {
-    float cronometro = 0;
+    public float cronometro = 0;
     // Start is called before the first frame update
     void Start()
     {
